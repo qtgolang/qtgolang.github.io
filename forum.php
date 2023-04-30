9b97e6b75348cfdaf846cc12c47d6b5e536c0dea
qtgolang <57537577+qtgolang@users.noreply.github.com>
2023-05-01 05:32:39 +0800

    r2

---
+++ forum.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>跳转到index.html</title>
+</head>
+<body>
+<script>
+window.location.href = "index.html";
+// 使用 JavaScript 实现页面跳转
+</script>
+</body>
+</html>
